c9e668bb8d759c4ece2b94ba41c2cc1061ab7174
michaelshel16 <113286433+michaelshel16@users.noreply.github.com>
2023-09-26 20:21:36 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -18,8 +18,8 @@ require('dotenv').config();
 const port              = process.env.PORT;
 const dataBaseUrl       = process.env.USERS_DATABASE_URL;
 app.use(cors({
-        credentials:true
-        origin:"https://6512ef1e9649bb12326419fc--thriving-cranachan-4a295d.netlify.app/"
+        credentials:true,
+        origin:"https://6512ef1e9649bb12326419fc--thriving-cranachan-4a295d.netlify.app"
 }));
 
         
