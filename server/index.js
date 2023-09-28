bec6c6d64681cc89394ac22e6b7bea094066d60f
michaelshel16 <113286433+michaelshel16@users.noreply.github.com>
2023-09-28 13:04:42 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -38,7 +38,7 @@ const storage = multer.diskStorage({
     
     destination:function(req,file,cb)
     {
-        cb(null,"./public/assets");
+        cb(null,"public/assets");
     },
     filename:function(req,file,cb)
     {
